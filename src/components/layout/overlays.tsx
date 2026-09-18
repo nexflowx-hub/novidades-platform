ff0d57e122f8e21eec03e4075492a274f792f3d0
NeXFlowX <nexflowx.tech@gmail.com>
2026-09-18 18:39:47 -0300

    perf: stop hydrating unfinished cart and account overlays

--- src/components/layout/overlays.tsx
+++ src/components/layout/overlays.tsx
@@ -1,22 +1,23 @@
 "use client";
 
 import { SearchOverlay } from "./search-overlay";
-import { CartDrawer } from "./cart-drawer";
 import { FavoritesDrawer } from "./favorites-drawer";
 import { MobileMenu } from "./mobile-menu";
-import { AccountDialog } from "./account-dialog";
 import { InfoDialog } from "./info-dialog";
 import { QuickViewDialog } from "./quick-view-dialog";
 
-/** Monta todos os overlays globais do Commerce (busca, carrinho, favoritos, etc). */
+/**
+ * Global launch overlays.
+ *
+ * Cart/account overlays are intentionally not mounted until their end-to-end
+ * flows are production-ready. Dedicated product funnels currently own checkout.
+ */
 export function Overlays() {
   return (
     <>
       <SearchOverlay />
-      <CartDrawer />
       <FavoritesDrawer />
       <MobileMenu />
-      <AccountDialog />
       <InfoDialog />
       <QuickViewDialog />
     </>
