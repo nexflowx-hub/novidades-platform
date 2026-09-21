fda882d61b44aced5e8702ed0d01912135cab6dd
NeXFlowX <nexflowx.tech@gmail.com>
2026-09-21 14:18:09 -0300

    feat: import site template catalog batch 05

---
+++ src/lib/site-templates/batch-05.ts
@@ -0,0 +1,521 @@
+import type { SiteTemplateSource } from "./types";
+export const SITE_TEMPLATE_BATCH_05: SiteTemplateSource[] = [
+  {
+    "slug": "garagem-e-mecanica-1",
+    "category": "33 - Garagem e Mecânica",
+    "categorySlug": "garagem-e-mecanica",
+    "sourceTitle": "1.json",
+    "sourceId": "1-1p-MSWReYPveYN92V8kWeCekK1f-Yoo"
+  },
+  {
+    "slug": "garagem-e-mecanica-2",
+    "category": "33 - Garagem e Mecânica",
+    "categorySlug": "garagem-e-mecanica",
+    "sourceTitle": "2.json",
+    "sourceId": "1vW4xAnLFjj5oBC1RAmAWClEkREpa9IXg"
+  },
+  {
+    "slug": "garagem-e-mecanica-3",
+    "category": "33 - Garagem e Mecânica",
+    "categorySlug": "garagem-e-mecanica",
+    "sourceTitle": "3.json",
+    "sourceId": "1kMaH7-UXT88VD2MP0SvENc03FoIw8NPk"
+  },
+  {
+    "slug": "garagem-e-mecanica-4",
+    "category": "33 - Garagem e Mecânica",
+    "categorySlug": "garagem-e-mecanica",
+    "sourceTitle": "4.json",
+    "sourceId": "1sBU_8IhfCqA5E-P34KYbvl6WrJb3RhGE"
+  },
+  {
+    "slug": "garagem-e-mecanica-5",
+    "category": "33 - Garagem e Mecânica",
+    "categorySlug": "garagem-e-mecanica",
+    "sourceTitle": "5.json",
+    "sourceId": "1ASxo56ea1u03gTemiQAhTjQmE82ytElZ"
+  },
+  {
+    "slug": "garagem-e-mecanica-6",
+    "category": "33 - Garagem e Mecânica",
+    "categorySlug": "garagem-e-mecanica",
+    "sourceTitle": "6.json",
+    "sourceId": "1fJW_Qd55GxsjihZ03tHDVz1IBFFvMPK7"
+  },
+  {
+    "slug": "garagem-e-mecanica-7",
+    "category": "33 - Garagem e Mecânica",
+    "categorySlug": "garagem-e-mecanica",
+    "sourceTitle": "7.json",
+    "sourceId": "17F0yF44HwRr0FCPraC3uuV8Yz53hzz7H"
+  },
+  {
+    "slug": "garagem-e-mecanica-8",
+    "category": "33 - Garagem e Mecânica",
+    "categorySlug": "garagem-e-mecanica",
+    "sourceTitle": "8.json",
+    "sourceId": "1WBVW3fzSdqNvFbrmHDjlR3ft3uM3qcHv"
+  },
+  {
+    "slug": "garagem-e-mecanica-9",
+    "category": "33 - Garagem e Mecânica",
+    "categorySlug": "garagem-e-mecanica",
+    "sourceTitle": "9.json",
+    "sourceId": "1id2BeI3SqIBqG9Qp5aTldQD1PcMvcpUv"
+  },
+  {
+    "slug": "garagem-e-mecanica-10",
+    "category": "33 - Garagem e Mecânica",
+    "categorySlug": "garagem-e-mecanica",
+    "sourceTitle": "10.json",
+    "sourceId": "1C0V3_Vqdvo5pharWGODOV4a1J1_28Y_E"
+  },
+  {
+    "slug": "hamburgueria-burguer-1",
+    "category": "34 - Hamburgueria",
+    "categorySlug": "hamburgueria",
+    "sourceTitle": "Burguer  (1).json",
+    "sourceId": "1oV1jMU5iZyUds5j19dUY-5CsK-M__KUb"
+  },
+  {
+    "slug": "hamburgueria-burguer-2",
+    "category": "34 - Hamburgueria",
+    "categorySlug": "hamburgueria",
+    "sourceTitle": "Burguer  (2).json",
+    "sourceId": "1YlG91VnklwsBRi0vCuerIdz_bedvxagw"
+  },
+  {
+    "slug": "hamburgueria-burguer-3",
+    "category": "34 - Hamburgueria",
+    "categorySlug": "hamburgueria",
+    "sourceTitle": "Burguer  (3).json",
+    "sourceId": "1m_2Yc7EOI69mt9-7dO_OAenTNhUn6IAz"
+  },
+  {
+    "slug": "hamburgueria-burguer-4",
+    "category": "34 - Hamburgueria",
+    "categorySlug": "hamburgueria",
+    "sourceTitle": "Burguer  (4).json",
+    "sourceId": "13U8SgTa2luVZLA2yCrhqxeDtoH1U32cP"
+  },
+  {
+    "slug": "hamburgueria-burguer-5",
+    "category": "34 - Hamburgueria",
+    "categorySlug": "hamburgueria",
+    "sourceTitle": "Burguer  (5).json",
+    "sourceId": "1VzHhW-iumPAVyZyW5g6po6NTut8s0LIT"
+  },
+  {
+    "slug": "hamburgueria-burguer-6",
+    "category": "34 - Hamburgueria",
+    "categorySlug": "hamburgueria",
+    "sourceTitle": "Burguer  (6).json",
+    "sourceId": "14yuI84US8UX5quDpUxUI2TZ4QsqtBGcm"
+  },
+  {
+    "slug": "hamburgueria-burguer-7",
+    "category": "34 - Hamburgueria",
+    "categorySlug": "hamburgueria",
+    "sourceTitle": "Burguer  (7).json",
+    "sourceId": "1K8PxuAImKTIUhU4ek041vqtsSg60KZDr"
+  },
+  {
+    "slug": "hamburgueria-burguer-8",
+    "category": "34 - Hamburgueria",
+    "categorySlug": "hamburgueria",
+    "sourceTitle": "Burguer  (8).json",
+    "sourceId": "14lTaNFPvxXlaUDtSbkVZ5GntW_I7a1If"
+  },
+  {
+    "slug": "hamburgueria-burguer-9",
+    "category": "34 - Hamburgueria",
+    "categorySlug": "hamburgueria",
+    "sourceTitle": "Burguer  (9).json",
+    "sourceId": "17syBMoT79qqblqPDOY0UZCJVNx1YzAba"
+  },
+  {
+    "slug": "hamburgueria-burguer-10",
+    "category": "34 - Hamburgueria",
+    "categorySlug": "hamburgueria",
+    "sourceTitle": "Burguer  (10).json",
+    "sourceId": "1g6rPmT0weRWIx_p-LFKxPTmoQSPyiQpR"
+  },
+  {
+    "slug": "hamburgueria-burguer-11",
+    "category": "34 - Hamburgueria",
+    "categorySlug": "hamburgueria",
+    "sourceTitle": "Burguer  (11).json",
+    "sourceId": "1pjgtzhOSgJIhOOUyxyFI_O2xrxHeTXHB"
+  },
+  {
+    "slug": "hamburgueria-burguer-12",
+    "category": "34 - Hamburgueria",
+    "categorySlug": "hamburgueria",
+    "sourceTitle": "Burguer  (12).json",
+    "sourceId": "1Hr1u9cRZWcP7TXYlJTR8UdqCjSigjcMR"
+  },
+  {
+    "slug": "hamburgueria-burguer-13",
+    "category": "34 - Hamburgueria",
+    "categorySlug": "hamburgueria",
+    "sourceTitle": "Burguer  (13).json",
+    "sourceId": "11gBeFC9i9heOCDnHOjbcoz7VwE8sNJoa"
+  },
+  {
+    "slug": "hamburgueria-burguer-14",
+    "category": "34 - Hamburgueria",
+    "categorySlug": "hamburgueria",
+    "sourceTitle": "Burguer  (14).json",
+    "sourceId": "1stFlJneCJFVOShhgTKIXo_hCgZ4w4lxL"
+  },
+  {
+    "slug": "igreja-igreja-1",
+    "category": "35 - Igreja",
+    "categorySlug": "igreja",
+    "sourceTitle": "Igreja (1).json",
+    "sourceId": "1VmOQzPv7z_galGlw11fhTAWBt_z31pVm"
+  },
+  {
+    "slug": "igreja-igreja-2",
+    "category": "35 - Igreja",
+    "categorySlug": "igreja",
+    "sourceTitle": "Igreja (2).json",
+    "sourceId": "1phYkELBtqZ-sMtu8WhsDnty92XehqdMB"
+  },
+  {
+    "slug": "igreja-igreja-3",
+    "category": "35 - Igreja",
+    "categorySlug": "igreja",
+    "sourceTitle": "Igreja (3).json",
+    "sourceId": "133NgAAOGIF75VM1WVa90akc5gXURNGX8"
+  },
+  {
+    "slug": "igreja-igreja-4",
+    "category": "35 - Igreja",
+    "categorySlug": "igreja",
+    "sourceTitle": "Igreja (4).json",
+    "sourceId": "1JnhuNtvjwFMR9uNUK1LUY6xMbwIeBnI3"
+  },
+  {
+    "slug": "igreja-igreja-5",
+    "category": "35 - Igreja",
+    "categorySlug": "igreja",
+    "sourceTitle": "Igreja (5).json",
+    "sourceId": "1H6wbxaWDdmIW3or3g0BJbzik3nm12AWO"
+  },
+  {
+    "slug": "igreja-igreja-6",
+    "category": "35 - Igreja",
+    "categorySlug": "igreja",
+    "sourceTitle": "Igreja (6).json",
+    "sourceId": "1CAk7HyPYqoogPnjOwU_aIUmwZhNVKrw9"
+  },
+  {
+    "slug": "igreja-igreja-7",
+    "category": "35 - Igreja",
+    "categorySlug": "igreja",
+    "sourceTitle": "Igreja (7).json",
+    "sourceId": "1CyiwAGKYwNubYOPhIYFzoznTeL0gst62"
+  },
+  {
+    "slug": "igreja-igreja-8",
+    "category": "35 - Igreja",
+    "categorySlug": "igreja",
+    "sourceTitle": "Igreja (8).json",
+    "sourceId": "1qQjfyexOm-XRSIrm0RT6FuOXlwZ-WLF2"
+  },
+  {
+    "slug": "igreja-igreja-9",
+    "category": "35 - Igreja",
+    "categorySlug": "igreja",
+    "sourceTitle": "Igreja (9).json",
+    "sourceId": "1vL8IvkZQEfFBNXgbZiYR-G-Dt7GBLda6"
+  },
+  {
+    "slug": "igreja-igreja-10",
+    "category": "35 - Igreja",
+    "categorySlug": "igreja",
+    "sourceTitle": "Igreja (10).json",
+    "sourceId": "133VjUCXMt42o6GCEycibIzA0SpYlJf5e"
+  },
+  {
+    "slug": "igreja-igreja-11",
+    "category": "35 - Igreja",
+    "categorySlug": "igreja",
+    "sourceTitle": "Igreja (11).json",
+    "sourceId": "1_XIDEQ_ranhLVz-ddRwuRWG962KiChGF"
+  },
+  {
+    "slug": "igreja-igreja-12",
+    "category": "35 - Igreja",
+    "categorySlug": "igreja",
+    "sourceTitle": "Igreja (12).json",
+    "sourceId": "1T3Ig5ypukZTqfFysRPuUP_Lqfq9-4Kop"
+  },
+  {
+    "slug": "igreja-igreja-13",
+    "category": "35 - Igreja",
+    "categorySlug": "igreja",
+    "sourceTitle": "Igreja (13).json",
+    "sourceId": "1pw_bPRc8FwScoyXCtzERReHAJyack406"
+  },
+  {
+    "slug": "igreja-igreja-14",
+    "category": "35 - Igreja",
+    "categorySlug": "igreja",
+    "sourceTitle": "Igreja (14).json",
+    "sourceId": "1mgy15nsWdyivubwryrCtFudcTqJPFxHN"
+  },
+  {
+    "slug": "igreja-igreja-15",
+    "category": "35 - Igreja",
+    "categorySlug": "igreja",
+    "sourceTitle": "Igreja (15).json",
+    "sourceId": "1cbFPAF-lqetb6WBNEiuJapCF5waY6eu6"
+  },
+  {
+    "slug": "igreja-igreja-16",
+    "category": "35 - Igreja",
+    "categorySlug": "igreja",
+    "sourceTitle": "Igreja (16).json",
+    "sourceId": "10y8EI8k7FSZC-ND4TkqelYAHP3yt9Ht2"
+  },
+  {
+    "slug": "igreja-igreja-17",
+    "category": "35 - Igreja",
+    "categorySlug": "igreja",
+    "sourceTitle": "Igreja (17).json",
+    "sourceId": "1EomvQCJxvNENNYV61r95gxPAEb4gk28u"
+  },
+  {
+    "slug": "ingles-5-pv-curso-de-ingles",
+    "category": "36 - Inglês",
+    "categorySlug": "ingles",
+    "sourceTitle": "5 PV Curso de inglês .json",
+    "sourceId": "15hxBCnUhZfproIh_rDaMyQUF020RRLN3"
+  },
+  {
+    "slug": "maquiagem-make-1",
+    "category": "37 - Maquiagem",
+    "categorySlug": "maquiagem",
+    "sourceTitle": "Make (1).json",
+    "sourceId": "15b_G5B-m4ALWpMhjAGEoqPuDj1qEQ1QI"
+  },
+  {
+    "slug": "maquiagem-make-3",
+    "category": "37 - Maquiagem",
+    "categorySlug": "maquiagem",
+    "sourceTitle": "Make (3).json",
+    "sourceId": "11oLAMaMeywi8iv3Rd2Z4nsW4pZnq2cZN"
+  },
+  {
+    "slug": "maquiagem-make-4",
+    "category": "37 - Maquiagem",
+    "categorySlug": "maquiagem",
+    "sourceTitle": "Make (4).json",
+    "sourceId": "1BbsUAE54ZiE5fs6tvLOajDAMkhIxYfC3"
+  },
+  {
+    "slug": "maquiagem-make-5",
+    "category": "37 - Maquiagem",
+    "categorySlug": "maquiagem",
+    "sourceTitle": "Make (5).json",
+    "sourceId": "1x8t2D88bZc_sEsmCt13G4mgRvI5vD7wz"
+  },
+  {
+    "slug": "maquiagem-make-6",
+    "category": "37 - Maquiagem",
+    "categorySlug": "maquiagem",
+    "sourceTitle": "Make (6).json",
+    "sourceId": "1AQCte7EtnudHU7n4dsGSJzuJecM2r-Px"
+  },
+  {
+    "slug": "maquiagem-make-7",
+    "category": "37 - Maquiagem",
+    "categorySlug": "maquiagem",
+    "sourceTitle": "Make (7).json",
+    "sourceId": "1sTo3in8nOCJ6X8vE9RoRsK348Qx88C66"
+  },
+  {
+    "slug": "maquiagem-make-8",
+    "category": "37 - Maquiagem",
+    "categorySlug": "maquiagem",
+    "sourceTitle": "Make (8).json",
+    "sourceId": "1QqbtSiDzxvwe2E-sUvt5zAyRHKdIw8XY"
+  },
+  {
+    "slug": "maquiagem-make-9",
+    "category": "37 - Maquiagem",
+    "categorySlug": "maquiagem",
+    "sourceTitle": "Make (9).json",
+    "sourceId": "1-S336o-U8znzG1IjgxmN0yCjUl2QPjVu"
+  },
+  {
+    "slug": "maquiagem-make-10",
+    "category": "37 - Maquiagem",
+    "categorySlug": "maquiagem",
+    "sourceTitle": "Make (10).json",
+    "sourceId": "15GuUG8zOKpnjXPg-3mlzQPBWIh5Q1QnB"
+  },
+  {
+    "slug": "maquiagem-make-11",
+    "category": "37 - Maquiagem",
+    "categorySlug": "maquiagem",
+    "sourceTitle": "Make (11).json",
+    "sourceId": "1QuFwwr5rC2AW9P4u9uvHQR9sqr0-MES-"
+  },
+  {
+    "slug": "maquiagem-make-12",
+    "category": "37 - Maquiagem",
+    "categorySlug": "maquiagem",
+    "sourceTitle": "Make (12).json",
+    "sourceId": "1kRn3yIrq3E4LvBG-rLAMeDknd9OLD9sf"
+  },
+  {
+    "slug": "maquiagem-make-13",
+    "category": "37 - Maquiagem",
+    "categorySlug": "maquiagem",
+    "sourceTitle": "Make (13).json",
+    "sourceId": "1OFQ-RB-5D6olZX3dHYn7tq3GNCc-1EGX"
+  },
+  {
+    "slug": "maquiagem-make-14",
+    "category": "37 - Maquiagem",
+    "categorySlug": "maquiagem",
+    "sourceTitle": "Make (14).json",
+    "sourceId": "1t-EwxM4SY0OqeCTzeVfhsAoiAc9cbCAI"
+  },
+  {
+    "slug": "maquiagem-make-15",
+    "category": "37 - Maquiagem",
+    "categorySlug": "maquiagem",
+    "sourceTitle": "Make (15).json",
+    "sourceId": "1h1itpg1RW_rcXMzfd2ihWyvTe_Mkz4i4"
+  },
+  {
+    "slug": "maquiadora-7-pagina-de-vendas-maquiadora",
+    "category": "38 - Maquiadora",
+    "categorySlug": "maquiadora",
+    "sourceTitle": "7 Página de vendas Maquiadora.json",
+    "sourceId": "19aWkST7nYCVDWWqWOZ1lFoyOJRiV9Oa_"
+  },
+  {
+    "slug": "marketing-digital-marketing-1",
+    "category": "39 - Marketing Digital",
+    "categorySlug": "marketing-digital",
+    "sourceTitle": "Marketing (1).json",
+    "sourceId": "1DVvHTWN7P0SPE0vEcBdB1P0X29ekLWHP"
+  },
+  {
+    "slug": "marketing-digital-marketing-2",
+    "category": "39 - Marketing Digital",
+    "categorySlug": "marketing-digital",
+    "sourceTitle": "Marketing (2).json",
+    "sourceId": "1PK6tzgiK0U_AdvN5flJm-Tmi6kqcS05c"
+  },
+  {
+    "slug": "marketing-digital-marketing-3",
+    "category": "39 - Marketing Digital",
+    "categorySlug": "marketing-digital",
+    "sourceTitle": "Marketing (3).json",
+    "sourceId": "1-uQR4_LdKqUPO1UJtGuomoaEQNky8591"
+  },
+  {
+    "slug": "marketing-digital-marketing-4",
+    "category": "39 - Marketing Digital",
+    "categorySlug": "marketing-digital",
+    "sourceTitle": "Marketing (4).json",
+    "sourceId": "1HY3RCPFV5XIfk2oMOwqP7gJqFMb86khY"
+  },
+  {
+    "slug": "marketing-digital-marketing-5",
+    "category": "39 - Marketing Digital",
+    "categorySlug": "marketing-digital",
+    "sourceTitle": "Marketing (5).json",
+    "sourceId": "1OfSYa5pjljCEUXJjF3hAD6AMLj7RTnOn"
+  },
+  {
+    "slug": "marketing-digital-marketing-6",
+    "category": "39 - Marketing Digital",
+    "categorySlug": "marketing-digital",
+    "sourceTitle": "Marketing (6).json",
+    "sourceId": "16TpPUlfqBuVLhUDpOlSQslDtghDz6oRS"
+  },
+  {
+    "slug": "marketing-digital-marketing-7",
+    "category": "39 - Marketing Digital",
+    "categorySlug": "marketing-digital",
+    "sourceTitle": "Marketing (7).json",
+    "sourceId": "1sJN9QrRrqqnCxGIOxInrqcZ96jN6I0D_"
+  },
+  {
+    "slug": "marketing-digital-marketing-8",
+    "category": "39 - Marketing Digital",
+    "categorySlug": "marketing-digital",
+    "sourceTitle": "Marketing (8).json",
+    "sourceId": "1qv4s-ZV5d5qr1UFWuhi73VBG_3fzV1pR"
+  },
+  {
+    "slug": "marketing-digital-marketing-9",
+    "category": "39 - Marketing Digital",
+    "categorySlug": "marketing-digital",
+    "sourceTitle": "Marketing (9).json",
+    "sourceId": "1UiHw4V_KnFldcGlfBWAz-faD0SrjDQW7"
+  },
+  {
+    "slug": "marketing-digital-marketing-10",
+    "category": "39 - Marketing Digital",
+    "categorySlug": "marketing-digital",
+    "sourceTitle": "Marketing (10).json",
+    "sourceId": "1KQq8msi7gd8UEIUYd6pHNOwzSj_W39bW"
+  },
+  {
+    "slug": "marketing-digital-marketing-11",
+    "category": "39 - Marketing Digital",
+    "categorySlug": "marketing-digital",
+    "sourceTitle": "Marketing (11).json",
+    "sourceId": "1jNCOGjs0FKecro67yiaAZDWjdRS-CLp7"
+  },
+  {
+    "slug": "marketing-digital-marketing-12",
+    "category": "39 - Marketing Digital",
+    "categorySlug": "marketing-digital",
+    "sourceTitle": "Marketing (12).json",
+    "sourceId": "10JglBN5lyOM1pxQz1VnrGyhbThr0AkE4"
+  },
+  {
+    "slug": "marketing-digital-marketing-13",
+    "category": "39 - Marketing Digital",
+    "categorySlug": "marketing-digital",
+    "sourceTitle": "Marketing (13).json",
+    "sourceId": "1RUX_XJfzH3GLtJ5-7Fr11QONwYO4H4ig"
+  },
+  {
+    "slug": "marketing-digital-marketing-14",
+    "category": "39 - Marketing Digital",
+    "categorySlug": "marketing-digital",
+    "sourceTitle": "Marketing (14).json",
+    "sourceId": "1lxsQYYteqtlgwCcs2h7MDlJNPOIYERt9"
+  },
+  {
+    "slug": "marketing-digital-marketing-15",
+    "category": "39 - Marketing Digital",
+    "categorySlug": "marketing-digital",
+    "sourceTitle": "Marketing (15).json",
+    "sourceId": "1syPXHRKtJt1XaStYOL7Jmd_f_1OduYEW"
+  },
+  {
+    "slug": "marketing-digital-marketing-16",
+    "category": "39 - Marketing Digital",
+    "categorySlug": "marketing-digital",
+    "sourceTitle": "Marketing (16).json",
+    "sourceId": "1LRPrqP4KX8E4OXq5rUMesvy8ZpvoBF5d"
+  },
+  {
+    "slug": "marketing-digital-13-marketing-digital",
+    "category": "40 - Marketing Digital",
+    "categorySlug": "marketing-digital",
+    "sourceTitle": "13 Marketing Digital.json",
+    "sourceId": "135dH2zvkWMjl7-Nwp0Etg-c2ssvDYtkx"
+  }
+];
